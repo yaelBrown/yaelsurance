d020100fa490edc8417999dfc8290bccb775fab0
yaelBrown <yaelrbrown@gmail.com>
2022-02-10 01:34:16 -0600

    feat: added nav and footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,49 @@
 import React from 'react';
+import Link from 'next/link'
+
+import styles from '../styles/Footer.module.css'
+
+const footerLinks = {
+  "Home": "/",
+  "Login": "/login",
+  "Register": "/register",
+  "Logout": "/logout"
+}
 
 export default function footer() {
-  return <div>foooooooter</div>;
+  return (
+    <footer id={styles.footer}>
+      <section id={styles.footerTop} className={styles.footerSection}/>
+
+      <section id={styles.footerMiddle} className={styles.footerSection}/>
+
+      <section id={styles.footerBottom} className={styles.footerSection}>
+        <div id={styles.footerBottomLeft}>
+          <ul>
+            { Object.keys(footerLinks).map((e,i) => {
+              return (
+                <Link key={i} href={footerLinks[e]}>
+                  <a>
+                    <li key={i}>{e}</li>
+                  </a>
+                </Link>
+              )
+            }) }
+          </ul>
+        </div>
+        <div id={styles.footerBottomRight}>
+            <Link href="https://github.com/yaelBrown/yaelsurance">
+              <a>
+                <h4>Yaelsurance</h4>
+              </a>
+            </Link>
+            <Link href="https://www.linkedin.com/in/yaelrbrown/">
+              <a>
+                <p>Created by Yael</p>
+              </a>
+            </Link>
+        </div>
+      </section>
+    </footer>
+  );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import Link from 'next/link'
+
+import { BsFillPersonFill } from 'react-icons/bs'
+
+import styles from '../styles/Navbar.module.css'
 
 export default function navbar() {
-  return <div>navbar</div>;
+  return (
+    <nav id={styles.nav}>
+      <section id={styles.navLeft}>
+        <Link href="/">
+          <a>
+            <h3>YaelSurance</h3>
+          </a>
+        </Link>
+      </section>
+
+      <section id={styles.navMiddle}></section>
+
+      <section id={styles.navRight}>
+        <Link href="/login">
+          <a>
+            <h3>Login</h3>
+          </a>
+        </Link>
+        <BsFillPersonFill 
+          id={styles.navRightIcon}
+          size="1.5em"
+        />
+      </section>
+    </nav>
+  );
 }
